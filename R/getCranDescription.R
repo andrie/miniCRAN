35ac167e6293ab2b2421cd706debacecc0970ae5
Andrie <apdevries@gmail.com>
2014-05-28 21:45:31 +0100

    Added tryCatch error checking to getCranDescription()

---
+++ R/getCranDescription.R
@@ -0,0 +1,36 @@
+#' Scrape DESCRIPTION from CRAN for each pkg.
+#' 
+#' @inheritParams pkgDep
+#' @inheritParams makeRepo
+#' @import XML
+#' @export
+#' @family miniCRAN
+#' @examples
+#' 
+#' getCranDescription(c("igraph", "ggplot2", "XML"), 
+#'   repos=c(CRAN="http://cran.revolutionanalytics.com")
+#' )
+getCranDescription <- function(pkg, repos=getOption("repos"), type="source", path, pkgs = pkgDep(pkg, repos=repos, type=type)){
+  
+  getOne <- function(package){
+    url <- sprintf("http://cran.r-project.org/web/packages/%s/index.html", package)
+    x <- tryCatch({
+      readHTMLTable(url, header=FALSE, which=1, stringsAsFactors=FALSE)
+    }, error=function(e) e
+    )
+    if(inherits(x, "error")) {
+      warning("Package ", package, " not found on CRAN")
+      NULL
+    } else {
+      names(x) <- c("Field", "Value")
+      x$Field <- gsub(":", "", x$Field)
+      x$Package <- package
+      x
+    }
+  }
+  ret <- do.call(rbind, lapply(pkgs, getOne))
+  ret <- reshape(ret, direction="wide", timevar="Field", idvar="Package", v.names="Value")
+  names(ret) <- gsub("Value.", "", names(ret))
+  rownames(ret) <- ret$Package
+  ret
+}
