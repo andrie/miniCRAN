b002c3412bed9376a466aaa8e7d2e5c37f989b1f
Andrie <apdevries@gmail.com>
2014-06-04 17:56:12 +0100

    Fixed makeRepo() and created makeLibrary()

--- R/makeRepo.R
+++ R/makeRepo.R
@@ -1,28 +1,55 @@
-#' Downloads packages from CRAN to specified path.
+#' Downloads packages from CRAN to specified path and creates repository or library.
 #' 
 #' Given a list of packages, downloads to a specified destination folder, then creates PACKAGES file.
+#' 
+#' The function \code{makeRepo} creates a repository, similar in structure to CRAN.  It optionally updates the PACKAGES file.  If done correctly, it is possible to use this folder as a repository, i.e. it will support functions like \code{install.packages}.
+#' 
+#' The function \code{makeLibrary} downloads the packages into a single folder, i.e. similar to a library on a machine.
 #' 
 #' Uses \code{\link{download.packages}} and \code{\link[tools]{write_PACKAGES}}
 #' 
 #' @inheritParams pkgDep
 #' @param pkgs Character vector of packages to download
-#' @param download If TRUE downloads packages, otherwise just creates PACKAGES file
 #' @param type Passed to \code{\link{download.packages}}
+#' @param download If TRUE downloads packages, otherwise just creates PACKAGES file
+#' @param writePACKAGES If TRUE, calls \code{\link[tools]{write_PACKAGES}} to update the repository PACKAGES file
 #' 
 #' @export
 #' @family miniCRAN
-makeRepo <- function(pkgs, path, download=FALSE, type="source"){
+makeRepo <- function(pkgs, path, type="source", download=FALSE, writePACKAGES=TRUE){
+  if(!file.exists(path)) stop("Download path does not exist")
+  if(type != "source") stop("At this time, only type=\"source\" is supported")
+  wd <- getwd()
+  on.exit(setwd(wd))
+  setwd(normalizePath(path))
+  
+  folder <- switch(type,
+                   "source" = "src/contrib",
+                   "win.binary" = "bin/windows/contrib/x.y",
+                   "mac.binary" = "bin/macosx/contrib/3.y",
+                   "mac.binary.mavericks" =  "bin/macosx/mavericks/contrib/3.y",
+                   "mac.binary.leopard"= "bin/macosx/leopard/contrib/2.y"
+  )
+  pkgPath <- file.path(path, folder)
+  if(!file.exists(pkgPath)) {
+    result <- dir.create(pkgPath, recursive=TRUE)
+    if(result) message("Creating new folders: ", pkgPath) else stop("Unable to create repo path: ", pkgPath)
+
+  }
+  
+  if(download) download.packages(pkgs, destdir=pkgPath, type=type)
+  if(writePACKAGES) tools::write_PACKAGES(dir=pkgPath, type=type) 
+}
+
+#' @rdname makeRepo
+#' @export
+makeLibrary <- function(pkgs, path, type="source"){
   if(!file.exists(path)) stop("Download path does not exist")
   wd <- getwd()
   on.exit(setwd(wd))
   setwd(normalizePath(path))
   message(getwd())
-  if(download) download.packages(pkgs, destdir=path, type=type)
-  tools::write_PACKAGES(dir=".", type=type) 
-}
-
-makeLibrary <- function(pkgs, path, download=FALSE, type="source"){
-  
+  download.packages(pkgs, destdir=path, type=type)
 }
 
 # downloadPackages <- function(pkg, path, type="source"){
