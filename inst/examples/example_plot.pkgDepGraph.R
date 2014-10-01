58f541bb6917fdc07391bed12462ea23f95ee51c
Andrie de Vries <apdevries@gmail.com>
2014-10-01 09:45:12 +0100

    Fixed placement of legend in example

--- inst/examples/example_plot.pkgDepGraph.R
+++ inst/examples/example_plot.pkgDepGraph.R
@@ -26,5 +26,5 @@ plot(dg, legendPosition=c(-1, 1), vertex.size=20,  cex=0.5)
 
 # Move vertex legend to top right
 set.seed(42); 
-plot(dg, legendPosition=c(-1, 1), vertex.size=20,  cex=0.5)
+plot(dg, legendPosition=c(1, 1), vertex.size=20,  cex=0.5)
 
