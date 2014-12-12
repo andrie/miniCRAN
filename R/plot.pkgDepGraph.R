d73135b630ef4f9d205532d913d070ff310935e1
Andrie de Vries <apdevries@gmail.com>
2014-12-12 17:23:00 +0000

    Added comment with unicode reference

--- R/plot.pkgDepGraph.R
+++ R/plot.pkgDepGraph.R
@@ -52,7 +52,7 @@ plot.pkgDepGraph <- function(
        xlim=c(-1.5, 1)
   )
   pch1 <- rep(19, length(plotColours))
-  pch2 <- rep(-8594, length(edgeColor))
+  pch2 <- rep(-8594, length(edgeColor))  #http://www.alanwood.net/unicode/arrows.html
   yjust <- function(x)0.5*(x+1)
   xjust <- function(x)1
 
