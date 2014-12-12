5afe49bf6d80ec6052d1b410fa4c91dad14f64df
Andrie de Vries <apdevries@gmail.com>
2014-12-12 08:32:25 +0000

    Separate vignettes into two files, rather than one file with two topics. Closes #35

---
+++ inst/doc/miniCRAN-dependency-graph.R
@@ -0,0 +1,20 @@
+## ----init----------------------------------------------------------------
+library(miniCRAN)
+
+## ----pkgdep--------------------------------------------------------------
+tags <- "chron"
+pkgDep(tags)
+
+## ----makeDepGraph, warning=FALSE-----------------------------------------
+dg <- makeDepGraph(tags, enhances=TRUE)
+set.seed(1)
+plot(dg, legendPosition = c(-1, 1), vertex.size=20)
+
+## ----so-tags, warning=FALSE, fig.width=10, fig.height=10-----------------
+tags <- c("ggplot2", "data.table", "plyr", "knitr", "shiny", "xts", "lattice")
+pkgDep(tags, suggests = TRUE, enhances=FALSE)
+
+dg <- makeDepGraph(tags, enhances=TRUE)
+set.seed(1)
+plot(dg, legendPosition = c(-1, -1), vertex.size=10, cex=0.7)
+
