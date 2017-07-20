7fc9d79d8501f444d9844b94eadb31e1e662c84a
Andrie de Vries <apdevries@gmail.com>
2017-07-20 22:54:05 +0100

    Make getCranDescription() backward compatible #91

--- R/getCranDescription.R
+++ R/getCranDescription.R
@@ -1,5 +1,38 @@
 #' Scrape DESCRIPTION from CRAN for each pkg.
 #'
+
+oldGetCranDescription <- function(pkg, repos = getOption("repos"), 
+                                  type = "source", 
+                                  pkgs = pkgDep(pkg, repos = repos, type = type)){
+  getOne <- function(package) {
+    repos <- repos[[1]]
+    if(!grepl("/$", repos)) repos <- paste0(repos, "/")
+    
+    url <- gsub("https://", "http://", 
+                sprintf("%sweb/packages/%s/index.html", repos, package)
+    )
+    x <- tryCatch({
+      text <- paste(readLines(url), collapse = "\n")
+      XML::readHTMLTable(text, header = FALSE, which = 1, stringsAsFactors = FALSE)
+    }, error = function(e) e
+    )
+    if(inherits(x, "error")) {
+      warning("Package ", package, " not found on CRAN")
+      NULL
+    } else {
+      names(x) <- c("Field", "Value")
+      x$Field <- gsub(":", "", x$Field)
+      x$Package <- package
+      x
+    }
+  }
+  ret <- do.call(rbind, lapply(pkgs, getOne))
+  ret <- reshape(ret, direction = "wide", timevar = "Field", idvar = "Package", v.names = "Value")
+  names(ret) <- gsub("Value.", "", names(ret))
+  rownames(ret) <- ret$Package
+  ret
+}
+
 #' @inheritParams pkgDep
 #' @inheritParams makeRepo
 #' 
@@ -10,7 +43,13 @@
 getCranDescription <- function(pkg, repos = getOption("repos"), 
                                type = "source", 
                                pkgs = pkgDep(pkg, repos = repos, type = type)){
-
-  pdb <- tools::CRAN_package_db()
-  pdb[match(pkgs, pdb$Package), ]
+  
+  if(getRversion() >= "3.4.1"){
+    pdb <- tools::CRAN_package_db()
+    pdb[match(pkgs, pdb$Package), ]
+  } else {
+    oldGetCranDescription(pkg = pkg, repos = repos, type = type, pkgs = pkgs)
+  }
 }
+
+
