dfe95cc043e634ede09850efcd22fd2c72ce2359
Andrie de Vries <apdevries@gmail.com>
2017-07-21 12:36:12 +0100

    Add repo to fix failing test

--- tests/testthat/test-6-getCranDescription.R
+++ tests/testthat/test-6-getCranDescription.R
@@ -4,7 +4,7 @@ context("get CRAN description")
 test_that("can read CRAN description", {
   
   skip_on_cran()
-  p <- getCranDescription("miniCRAN")
+  p <- getCranDescription("miniCRAN", repos = c(CRAN = "https://mran.microsoft.com"))
   expect_is(p, "data.frame")
   expect_equal(p$Package[1], "miniCRAN")
 
