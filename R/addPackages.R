4ab7a2c2d88aa5cc4ea9feda35f63a9af359723a
Brian Mayer <33881607+bleemayer@users.noreply.github.com>
2018-09-17 22:11:29 -0300

    Added $source to prev
     solves #99.

--- R/addPackages.R
+++ R/addPackages.R
@@ -285,7 +285,7 @@ addLocalPackage <- function(pkgs = NULL, pkgPath = NULL, path = NULL,
     files <- .listFiles(pkgs = pkgs, path = pkgPath, type = t)
 
     # check for previous package version and omit if identical
-    prev <- checkVersions(pkgs, path)
+    prev <- checkVersions(pkgs, path)$source
     same <- which(basename(as.character(prev)) %in% files)
 
     if (length(same)) {
