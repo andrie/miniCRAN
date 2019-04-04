e1a7f2bc6fdd2a79058a26cdd151c1a688f8c9ef
Anton Antonov <antonov@devexperts.com>
2019-04-04 18:50:15 +0300

    Refactor dependency handling (deletePackage)

--- R/deletePackages.R
+++ R/deletePackages.R
@@ -1,36 +1,39 @@
 #' Remove packages from a miniCRAN repository.
 #' @inheritParams addPackage
 #' @param deps logical indicating whether the package reverse dependencies should be removed (default `TRUE`).
-#' @param recursive (only if `deps = TRUE`) logical indicating whether dependencies should be removed recursively (default `TRUE`)
-#' @importFrom tools package_dependencies
+#' @param ... arguments passed over to `tools::package_dependencies`: use `which`, `recursive`, `reverse`
+#' to specify what should happen to dependencies of removed packages
 #' @export
 #' @family update repo functions
-deletePackage <- function(pkgs = NULL, path = NULL, # repos = getOption("repos"), -- needed for pkgDep
+deletePackage <- function(pkgs = NULL, path = NULL,
                           type = "source", Rversion = R.version,
-                          writePACKAGES = TRUE, deps = TRUE, recursive = TRUE) {
+                          writePACKAGES = TRUE, deps = TRUE, ...) {
     if (is.null(path) || is.null(pkgs)) stop("path and pkgs must both be specified.")
 
     sapply(type, function(t) {
         repoPath <- file.path(path, repoPrefix(t, Rversion))
+        db <- pkgAvail(repos = path, type = t, Rversion = Rversion)
+        purgePackage(pkgs, db, t, repoPath)
 
-        a <- pkgAvail(repos = path, type = t, Rversion = Rversion)
-        w <- which(a[, "Package"] %in% pkgs)
-        if (length(w)) {
-            ext <- switch(t, source = ".tar.gz", mac.binary = ".tgz", win.binary = ".zip")
-            toRemove <- paste0(a[w, "Package"], "_", a[w, "Version"], ext)
-            unlink(file.path(path, repoPath, toRemove))
-
-            if (deps) {
-                d <- package_dependencies(pkgs, a, reverse = TRUE, recursive = recursive)
-                depends <- if (!is.null(d[[pkgs]])) d[[pkgs]] else character()
-                needed <- unique(unlist(d[!names(d) %in% c(pkgs, depends)]))
-                toRemoveDeps <- depends[!depends %in% needed]
-                w <- which(a[, "Package"] %in% toRemoveDeps)
-                toRemoveDeps <- paste0(a[w, "Package"], "_", a[w, "Version"], ext)
-                unlink(file.path(path, repoPath, toRemoveDeps))
-            }
+        if (deps) {
+            d <- tools::package_dependencies(pkgs, db, ...)
+            depends <- if (!is.null(d[[pkgs]])) d[[pkgs]] else character()
+            needed <- unique(unlist(d[!names(d) %in% c(pkgs, depends)]))
+            toRemoveDeps <- depends[!depends %in% needed]
+            purgePackage(toRemoveDeps, db, t, repoPath)
         }
     })
 
     if (writePACKAGES) invisible(updateRepoIndex(path = path, type = type, Rversion = Rversion))
+}
+
+# given packages, reconstruct file names and remove these files
+purgePackage <- function(pkgs, db, type, repoPath) {
+    ext <- switch(type, source = ".tar.gz", mac.binary = ".tgz", win.binary = ".zip")
+    w <- which(db[, "Package"] %in% pkgs)
+
+    if (length(w)) {
+        toRemove <- paste0(db[w, "Package"], "_", db[w, "Version"], ext)
+        unlink(file.path(repoPath, toRemove))
+    }
 }
