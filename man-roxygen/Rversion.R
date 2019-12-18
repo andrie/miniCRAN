5b898d4b1008def01d818648aa51446f6d2aa0f2
Alex Chubaty <alex.chubaty@gmail.com>
2019-12-18 09:02:09 -0700

    Rversion doc fix

--- man-roxygen/Rversion.R
+++ man-roxygen/Rversion.R
@@ -1,6 +1,6 @@
 #' @param Rversion Version of R (only used if `type` is not `source`.) Defaults to [R.version], but this can be specified as any of the following formats:
-#' 
+#'
 #'   * a character string with the two digit R version, e.g. "3.1"
 #'   * a list with components `major` and `minor`
 #'   * the result of [getRversion()]
-#'   * the result of [R.version()]
+#'   * the result of [R.version]
