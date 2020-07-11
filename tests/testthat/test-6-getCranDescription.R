d4a762ee14bf60bd481e5650ca3e910870665ae8
Andrie <apdevries@gmail.com>
2020-07-11 20:40:50 +0000

    Mock version of R to test getCranDescription() on old versions of R

--- tests/testthat/test-6-getCranDescription.R
+++ tests/testthat/test-6-getCranDescription.R
@@ -5,26 +5,31 @@ test_that("can read CRAN description", {
 
   skip_on_cran()
   skip_if(getRversion() <= "3.4")
-  
+
   skip_if_not_installed("mockery")
-  mockery::stub(getCranDescription, 
-                what = "tools::CRAN_package_db",
-                function(...) testthis::read_testdata("/pdb.rds")
-                )
+  mockery::stub(
+    getCranDescription,
+    what = "tools::CRAN_package_db",
+    function(...) testthis::read_testdata("/pdb.rds")
+  )
   p <- getCranDescription("miniCRAN", repos = c(CRAN = getOption("minicran.mran")))
   expect_is(p, "data.frame")
   expect_equal(p$Package[1], "miniCRAN")
 })
 
-
-
 test_that("throws error on old versions of R", {
-  
-  skip_on_cran()
-  skip_if(getRversion() > "3.4", "Skipping on R>3.4")
-  
+  if (getRversion() > "3.4.0") {
+    # mock the version of R to force the error
+    mockery::stub(
+      getCranDescription, 
+      what = "getRversion",
+      function() "3.4.0",
+    )
+  }
   expect_error(
-    getCranDescription("miniCRAN")
+    getCranDescription("miniCRAN", repos = c(CRAN = getOption("minicran.mran"))),
+    "This function is not available"
   )
-  
 })
+  
+
